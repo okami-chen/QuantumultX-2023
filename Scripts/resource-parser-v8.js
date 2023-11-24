55c99a3154a8c9e5362a18b32350c490368fcd6c
okamichen <webmaster@88188.live>
2023-11-24 00:35:38 -0600

    Update and rename resource-parser-v7.js to resource-parser-v8.js

--- Scripts/resource-parser-v8.js
+++ Scripts/resource-parser-v8.js
@@ -240,7 +240,13 @@ var ProfileInfo = {
 }
 
 function addSpaceBetweenAlphaNumeric(input) {
-    return input.replace(/([A-Za-z])(\d+)/g, (match, p1, p2) => p1 + ' ' + p2.padStart(2, '0'));
+    return input.replace(/([A-Za-z])(\d+)?/g, (match, p1, p2) => {
+        if (p2) {
+            return p1 + ' - ' + p2.padStart(2, '0');
+        } else {
+            return p1 + ' - 01';
+        }
+    });
 }
 function serverNameReplace(tag){
     var rr = {
