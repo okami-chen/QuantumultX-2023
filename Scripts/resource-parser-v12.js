6f9def9128db50a3e486ae6548a3c1b2a1b5b3c2
okamichen <webmaster@88188.live>
2023-12-06 08:59:52 +0800

    Update resource-parser-v12.js

--- Scripts/resource-parser-v12.js
+++ Scripts/resource-parser-v12.js
@@ -358,6 +358,10 @@ function serverNameReplace(tag){
         "España":"西班牙",
         "Italia":"意大利",
         "Colombia":"哥伦比亚",
+        "Mumbai":"印度 孟买"，
+        "Amsterdam": "荷兰 阿姆斯特丹",
+        "Aadrid":"西班牙 马德里",
+         "AE":"阿拉伯联合酋长国",
     }
 
     tag = addSpaceBetweenAlphaNumeric(tag)
