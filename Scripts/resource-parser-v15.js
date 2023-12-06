9c825d7440dadcb32a24f9ef6cef14f1ef94d517
okamichen <webmaster@88188.live>
2023-12-06 09:04:31 +0800

    Update and rename resource-parser-v13.js to resource-parser-v15.js

--- Scripts/resource-parser-v15.js
+++ Scripts/resource-parser-v15.js
@@ -360,8 +360,7 @@ function serverNameReplace(tag){
         "Colombia":"哥伦比亚",
         "Mumbai":"印度 孟买"，
         "Amsterdam": "荷兰 阿姆斯特丹",
-        "Aadrid":"西班牙 马德里",
-        "AE":"阿拉伯联合酋长国"
+        "Aadrid":"西班牙 马德里"
     }
 
     tag = addSpaceBetweenAlphaNumeric(tag)
