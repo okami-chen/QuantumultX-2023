80e292683eeac2702011ca32900e3999f204aab6
okamichen <webmaster@88188.live>
2023-12-13 11:29:15 +0800

    Update and rename IP-API-V2.js to IP-API-V6.js

--- Scripts/IP-API-V6.js
+++ Scripts/IP-API-V6.js
@@ -40,30 +40,65 @@ function Area_check(para) {
 
 var flags = new Map([[ "AC" , "🇦🇨" ] ,["AE","🇦🇪"], [ "AF" , "🇦🇫" ] , [ "AI" , "🇦🇮" ] , [ "AL" , "🇦🇱" ] , [ "AM" , "🇦🇲" ] , [ "AQ" , "🇦🇶" ] , [ "AR" , "🇦🇷" ] , [ "AS" , "🇦🇸" ] , [ "AT" , "🇦🇹" ] , [ "AU" , "🇦🇺" ] , [ "AW" , "🇦🇼" ] , [ "AX" , "🇦🇽" ] , [ "AZ" , "🇦🇿" ] , ["BA", "🇧🇦"], [ "BB" , "🇧🇧" ] , [ "BD" , "🇧🇩" ] , [ "BE" , "🇧🇪" ] , [ "BF" , "🇧🇫" ] , [ "BG" , "🇧🇬" ] , [ "BH" , "🇧🇭" ] , [ "BI" , "🇧🇮" ] , [ "BJ" , "🇧🇯" ] , [ "BM" , "🇧🇲" ] , [ "BN" , "🇧🇳" ] , [ "BO" , "🇧🇴" ] , [ "BR" , "🇧🇷" ] , [ "BS" , "🇧🇸" ] , [ "BT" , "🇧🇹" ] , [ "BV" , "🇧🇻" ] , [ "BW" , "🇧🇼" ] , [ "BY" , "🇧🇾" ] , [ "BZ" , "🇧🇿" ] , [ "CA" , "🇨🇦" ] , [ "CF" , "🇨🇫" ] , [ "CH" , "🇨🇭" ] , [ "CK" , "🇨🇰" ] , [ "CL" , "🇨🇱" ] , [ "CM" , "🇨🇲" ] , [ "CN" , "🇨🇳" ] , [ "CO" , "🇨🇴" ] , [ "CP" , "🇨🇵" ] , [ "CR" , "🇨🇷" ] , [ "CU" , "🇨🇺" ] , [ "CV" , "🇨🇻" ] , [ "CW" , "🇨🇼" ] , [ "CX" , "🇨🇽" ] , [ "CY" , "🇨🇾" ] , [ "CZ" , "🇨🇿" ] , [ "DE" , "🇩🇪" ] , [ "DG" , "🇩🇬" ] , [ "DJ" , "🇩🇯" ] , [ "DK" , "🇩🇰" ] , [ "DM" , "🇩🇲" ] , [ "DO" , "🇩🇴" ] , [ "DZ" , "🇩🇿" ] , [ "EA" , "🇪🇦" ] , [ "EC" , "🇪🇨" ] , [ "EE" , "🇪🇪" ] , [ "EG" , "🇪🇬" ] , [ "EH" , "🇪🇭" ] , [ "ER" , "🇪🇷" ] , [ "ES" , "🇪🇸" ] , [ "ET" , "🇪🇹" ] , [ "EU" , "🇪🇺" ] , [ "FI" , "🇫🇮" ] , [ "FJ" , "🇫🇯" ] , [ "FK" , "🇫🇰" ] , [ "FM" , "🇫🇲" ] , [ "FO" , "🇫🇴" ] , [ "FR" , "🇫🇷" ] , [ "GA" , "🇬🇦" ] , [ "GB" , "🇬🇧" ] , [ "HK" , "🇭🇰" ] ,["HU","🇭🇺"], [ "ID" , "🇮🇩" ] , [ "IE" , "🇮🇪" ] , [ "IL" , "🇮🇱" ] , [ "IM" , "🇮🇲" ] , [ "IN" , "🇮🇳" ] , [ "IS" , "🇮🇸" ] , [ "IT" , "🇮🇹" ] , [ "JP" , "🇯🇵" ] , [ "KR" , "🇰🇷" ] , [ "LU" , "🇱🇺" ] , [ "MO" , "🇲🇴" ] , [ "MX" , "🇲🇽" ] , [ "MY" , "🇲🇾" ] , [ "NL" , "🇳🇱" ] , [ "PH" , "🇵🇭" ] , [ "RO" , "🇷🇴" ] , [ "RS" , "🇷🇸" ] , [ "RU" , "🇷🇺" ] , [ "RW" , "🇷🇼" ] , [ "SA" , "🇸🇦" ] , [ "SB" , "🇸🇧" ] , [ "SC" , "🇸🇨" ] , [ "SD" , "🇸🇩" ] , [ "SE" , "🇸🇪" ] , [ "SG" , "🇸🇬" ] , [ "TH" , "🇹🇭" ] , [ "TN" , "🇹🇳" ] , [ "TO" , "🇹🇴" ] , [ "TR" , "🇹🇷" ] , [ "TV" , "🇹🇻" ] , [ "TW" , "🇨🇳" ] , [ "UK" , "🇬🇧" ] , [ "UM" , "🇺🇲" ] , [ "US" , "🇺🇸" ] , [ "UY" , "🇺🇾" ] , [ "UZ" , "🇺🇿" ] , [ "VA" , "🇻🇦" ] , [ "VE" , "🇻🇪" ] , [ "VG" , "🇻🇬" ] , [ "VI" , "🇻🇮" ] , [ "VN" , "🇻🇳" ] , [ "ZA" , "🇿🇦"], ["UA" , "🇺🇦"], [ "MD" , "🇲🇩"]])
 var body = $response.body;
-var obj = JSON.parse(body);
-var title = flags.get(obj['countryCode']) + ' '+obj['country'] + ' ' + obj['regionName']
-
-var subtitle = obj['city'] + ' ' + obj['query'];
+var t = JSON.parse(body);
 var ip = obj['query'];
-var description = '\n';
-description = description + 'IP: '+ obj['query'] + '\n\n';
-description = description + '服务商: '+ obj['isp'] + '\n\n';
-description = description + '组织: '+ obj['asname'] + '\n\n';
 
-if (obj['hosting'] == false || obj['mobile'] == false){
-  description = description + '类型: 家宽\n\n';
-  title = title + ' 家宽'
-}else{
-  description = description + '类型: 机房\n\n';
-}
+var url = "http://ip-api.com/json/"+ip+"?ang=zh-CN&fields=message,mobile,proxy,hosting,query"
+var opts = {
+  policy: $environment.params
+};
+var myRequest = {
+  url: url,
+  opts: opts,
+  timeout: 4000
+};
 
-description = description + '国家: '+ obj['country'] + '\n\n';
-description = description + '地区: '+ obj['regionName'] + '\n\n';
-description = description + '城市: '+ City_ValidCheck(obj['city']) + '\n\n';
-description = description + '邮编: '+ obj['zip'] + '\n\n';
-description = description + '时区: '+ obj['timezone'] + '\n\n';
-description = description + '经度: '+ obj['lon'] + '\n\n';
-description = description + '纬度: '+ obj['lat'] + '\n\n';
-$done({title, subtitle, ip, description});
+
+$task.fetch(myRequest).then(response => {
+  var ret = JSON.parse(response.body)
+  var title = flags.get(obj['countryCode']) + ' '+obj['country'] + ' ' + obj['regionName']
+  var subtitle = obj['city'] + ' ' + obj['query'];
+  var description = '\n';
+  description = description + 'IP: '+ obj['query'] + '\n\n';
+  description = description + '服务商: '+ obj['isp'] + '\n\n';
+  description = description + '组织: '+ obj['asname'] + '\n\n';
+  if (ret['hosting'] == false || ret['mobile'] == false){
+    description = description + '类型: 家宽\n\n';
+    title = title + ' 家宽'
+  }else{
+    description = description + '类型: 机房\n\n';
+  }
+  description = description + '国家: '+ obj['country'] + '\n\n';
+  description = description + '地区: '+ obj['regionName'] + '\n\n';
+  description = description + '城市: '+ City_ValidCheck(obj['city']) + '\n\n';
+  description = description + '邮编: '+ obj['zip'] + '\n\n';
+  description = description + '时区: '+ obj['timezone'] + '\n\n';
+  description = description + '经度: '+ obj['lon'] + '\n\n';
+  description = description + '纬度: '+ obj['lat'] + '\n\n';
+  $done({title, subtitle, ip, description});
+}, reason => {
+  var title = flags.get(obj['countryCode']) + ' '+obj['country'] + ' ' + obj['regionName']
+  var subtitle = obj['city'] + ' ' + obj['query'];
+  var description = '\n';
+  description = description + 'IP: '+ obj['query'] + '\n\n';
+  description = description + '服务商: '+ obj['isp'] + '\n\n';
+  description = description + '组织: '+ obj['asname'] + '\n\n';
+  if (obj['hosting'] == false || obj['mobile'] == false){
+    description = description + '类型: 家宽\n\n';
+    title = title + ' 家宽'
+  }else{
+    description = description + '类型: 机房\n\n';
+  }
+  description = description + '国家: '+ obj['country'] + '\n\n';
+  description = description + '地区: '+ obj['regionName'] + '\n\n';
+  description = description + '城市: '+ City_ValidCheck(obj['city']) + '\n\n';
+  description = description + '邮编: '+ obj['zip'] + '\n\n';
+  description = description + '时区: '+ obj['timezone'] + '\n\n';
+  description = description + '经度: '+ obj['lon'] + '\n\n';
+  description = description + '纬度: '+ obj['lat'] + '\n\n';
+  $done({title, subtitle, ip, description});
+})
+
+
 
 
