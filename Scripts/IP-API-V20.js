7e417e717f8bdf683f48194ad50b8ac5f5fe6af6
okamichen <webmaster@88188.live>
2023-12-15 16:55:37 +0800

    Update IP-API-V20.js

--- Scripts/IP-API-V20.js
+++ Scripts/IP-API-V20.js
@@ -39,23 +39,22 @@ function Area_check(para) {
 }
 
 function replaceOrganization(name) {
-  const tmp = name.split(' ');
-  const t = tmp[0].split('-');
-  const replacements = {
-    'POLONETWORK': 'Tanaka',
-    'SERVERS': '',
-    'SERVER': '',
-    'NETWORKS': '',
-    'CLOUDFLARENET' : 'CLOUDFLARE',
-  };
-
-  for (const key in replacements) {
-    if (Object.prototype.hasOwnProperty.call(replacements, key)) {
-      t[0] = t[0].replace(new RegExp(key, 'gi'), replacements[key]);
+    const tmp = name.split(' ');
+    const t = tmp[0].split('-');
+    const replacements = {
+        'POLONETWORK': 'Tanaka',
+        'SERVERS': '',
+        'SERVER': '',
+        'NETWORKS': '',
+        'CLOUDFLARENET': 'CLOUDFLARE',
+    };
+    for (const key in replacements) {
+        if (replacements.hasOwnProperty(key)) {
+            const regex = new RegExp(key, 'g');
+            tmp[0] = tmp[0].replace(regex, replacements[key]);
+        }
     }
-  }
-
-  return t[0].charAt(0).toUpperCase() + t[0].slice(1).toLowerCase();
+    return tmp[0].charAt(0).toUpperCase() + tmp[0].slice(1).toLowerCase();
 }
 
 
@@ -89,20 +88,21 @@ var body = $response.body;
 var obj = JSON.parse(body);
 var ip = obj['query'];
 
-var title = flags.get(obj['countryCode']) + ' '+obj['country'] + ' ' + obj['city'] + ' ' +replaceOrganization(obj['isp'])
-var subtitle = obj['regionName'] + ' ' + obj['query'];
+var title = flags.get(obj['countryCode']) + ' ' + obj['country']  + ' ' + replaceOrganization(obj['isp'])
+if(obj['country'] == obj['city']){
+  var subtitle = obj['regionName']+ ' ' + obj['query'];
+}else{
+  var subtitle = obj['regionName']+ ' ' + obj['city'] + ' ' + obj['query'];
+}
+
 var description = '\n';
-description = description + 'IP: '+ obj['query'] + '\n';
-description = description + '服务商: '+ obj['isp'] + '\n';
-description = description + '国家: '+ obj['country'] + '\n';
-description = description + '地区: '+ obj['regionName'] + '\n';
-description = description + '城市: '+ City_ValidCheck(obj['city']) + '\n';
-description = description + '邮编: '+ obj['zip'] + '\n';
-description = description + '时区: '+ obj['timezone'] + '\n';
-description = description + '经度: '+ obj['lon'] + '\n';
-description = description + '纬度: '+ obj['lat'] + '\n';
+description = description + 'IP: '+ obj['query'] + '\n\n';
+description = description + '服务商: '+ obj['isp'] + '\n\n';
+description = description + '国家: '+ obj['country'] + '\n\n';
+description = description + '地区: '+ obj['regionName'] + '\n\n';
+description = description + '城市: '+ City_ValidCheck(obj['city']) + '\n\n';
+description = description + '邮编: '+ obj['zip'] + '\n\n';
+description = description + '时区: '+ obj['timezone'] + '\n\n';
+description = description + '经度: '+ obj['lon'] + '\n\n';
+description = description + '纬度: '+ obj['lat'] + '\n\n';
 $done({title, subtitle, ip, description});
-
-
-
-
